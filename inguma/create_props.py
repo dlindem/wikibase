ab3b5008a3c87f20a41b7fd1682d48578b6c2798
David Lindemann <34068491+dlindem@users.noreply.github.com>
2022-03-18 17:47:13 +0100

    Create create_props.py

---
+++ inguma/create_props.py
@@ -0,0 +1,46 @@
+import iwbi, csv
+
+datatype_mapping = {'commonsMedia': 'http://wikiba.se/ontology#CommonsMedia' ,
+                'external-id': 'http://wikiba.se/ontology#ExternalId' ,
+                'geo-shape': 'http://wikiba.se/ontology#GeoShape',
+                'globe-coordinate': 'http://wikiba.se/ontology#GlobeCoordinate',
+                'math': 'http://wikiba.se/ontology#Math',
+                'monolingualtext': 'http://wikiba.se/ontology#Monolingualtext',
+                'quantity': 'http://wikiba.se/ontology#Quantity',
+                'string': 'http://wikiba.se/ontology#String',
+                'tabular-data': 'http://wikiba.se/ontology#TabularData',
+                'time': 'http://wikiba.se/ontology#Time',
+                'edtf': '<http://wikiba.se/ontology#Edtf>',
+                'url': 'http://wikiba.se/ontology#Url',
+                'wikibase-item': 'http://wikiba.se/ontology#WikibaseItem',
+                'wikibase-property': 'http://wikiba.se/ontology#WikibaseProperty',
+                'lexeme': 'http://wikiba.se/ontology#WikibaseLexeme',
+                'form': 'http://wikiba.se/ontology#WikibaseForm',
+                'sense': 'http://wikiba.se/ontology#WikibaseSense',
+                'musical-notation': 'http://wikiba.se/ontology#MusicalNotation',
+                'schema': 'http://schema.org/'
+                }
+
+
+with open('props.csv', 'r', encoding="utf-8") as csvfile:
+	props = csv.DictReader(csvfile)
+
+	for prop in props:
+		print("\n"+str(prop))
+		for dtype in datatype_mapping:
+			if datatype_mapping[dtype] == prop['datatype']:
+				datatype = dtype
+
+		data = []
+
+		if prop['wd'].startswith("P"):
+			data.append(iwbi.wbi_datatype.ExternalID(value=prop['wd'], prop_nr="P1"))
+		if prop['formUrl'].startswith("http"):
+			data.append(iwbi.wbi_datatype.String(value=prop['formUrl'], prop_nr="P2"))
+
+
+		wb_item = iwbi.wbi_core.ItemEngine(new_item=True,data=data)
+		wb_item.set_label(prop['propLabel'], lang="en", if_exists='REPLACE')
+		result = wb_item.write(iwbi.login_instance, entity_type="property", property_datatype=datatype)
+
+		print(str(result), "done.")
