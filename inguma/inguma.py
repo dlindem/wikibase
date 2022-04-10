8046c11b610a820da1bdd84c8f57b9842da2909c
David Lindemann <34068491+dlindem@users.noreply.github.com>
2022-04-10 23:58:10 +0200

    Update inguma.py

--- inguma/inguma.py
+++ inguma/inguma.py
@@ -3,29 +3,146 @@ import config_private
 
 mappings = {
 "persons":
-	{
-	"id": "str:P49",
+{
+	"classqid": "Q5",
+	"classdesc": "Pertsona bat",
+	"id": "extra:",
 	"name": "str:P6",
 	"surname": "str:P8",
-	"secondSurname": "str:P9",
+	"secondSurname": "extra:", # extra
 	"cleanName": "ext:P3",
 	"oRCIDCode": "ext:P39",
 	"generoa": "item:gender"
 },
 "item:gender":
-	{
+{
 	"emakumezkoa": "P50_Q31",
 	"gizonezkoa": "P50_Q32",
 	"beste generoak": "P50_Q33"
-	}
+
+},
+"productions":
+{
+	"classqid": "Q3",
+	"id": "extra:",
+    "title": "mon:eu:P10",
+    "cleanTitle": "str:P12",
+    "type": "item:ingumaProdType",
+    "year": "extra:",
+    "shu": "item:UDC",
+    "url": "url:P24",
+    "organizationId": "extra:",
+    "issue": "str:P26",
+    "firstPage": "str:P27",
+    "lastPage": "str:P28",
+    "bookTitle": "str:P54",
+    "writerName": "extra:", # FEHLT
+    "isbn": "extra:", # extra
+    "doi": "extra:" # extra
+},
+"organizations":
+{
+	#"classqid": "Q6", # instance-of statement is done using "type"
+	"id": "extra:",
+    "name": "str:P56",
+    "type": "item:ingumaOrgType",
+    "address": "extra:",
+    "web": "url:P41",
+    "issn": "extra:",
+    "cleanName": "str:P11",
+},
+"item:ingumaProdType":
+{
+
+},
+"item:ingumaOrgType": # this is translated to class Q6 (org) or class Q7 (journal)
+{
+"magazine": "P5_Q7",
+"publisher": "P5_Q6",
+"other": "P5_Q6",
+"university": "P5_Q6"
+},
+"item:UDC":
+{
+	"0": "P52_Q6535",
+	"01": "P52_Q6536",
+	"02": "P52_Q6537",
+	"03": "P52_Q6538",
+	"05": "P52_Q6539",
+	"06": "P52_Q6540",
+	"07": "P52_Q6541",
+	"08": "P52_Q6542",
+	"09": "P52_Q6543",
+	"1": "P52_Q6544",
+	"11": "P52_Q6545",
+	"13": "P52_Q6546",
+	"14": "P52_Q6547",
+	"15": "P52_Q6548",
+	"16": "P52_Q6549",
+	"17": "P52_Q6550",
+	"2": "P52_Q6551",
+	"3": "P52_Q6552",
+	"30": "P52_Q6553",
+	"31": "P52_Q6554",
+	"32": "P52_Q6555",
+	"33": "P52_Q6556",
+	"34": "P52_Q6557",
+	"35": "P52_Q6558",
+	"36": "P52_Q6559",
+	"37": "P52_Q6560",
+	"39": "P52_Q6561",
+	"4": "P52_Q6562",
+	"41": "P52_Q6563",
+	"42": "P52_Q6564",
+	"43": "P52_Q6565",
+	"44": "P52_Q6566",
+	"5": "P52_Q6567",
+	"51": "P52_Q6568",
+	"52": "P52_Q6569",
+	"53": "P52_Q6570",
+	"54": "P52_Q6571",
+	"55": "P52_Q6572",
+	"56": "P52_Q6573",
+	"57": "P52_Q6574",
+	"58": "P52_Q6575",
+	"59": "P52_Q6576",
+	"6": "P52_Q6577",
+	"61": "P52_Q6578",
+	"62": "P52_Q6579",
+	"63": "P52_Q6580",
+	"64": "P52_Q6581",
+	"65": "P52_Q6582",
+	"66": "P52_Q6583",
+	"67": "P52_Q6584",
+	"68": "P52_Q6585",
+	"69": "P52_Q6586",
+	"7": "P52_Q6587",
+	"71": "P52_Q6588",
+	"72": "P52_Q6589",
+	"73": "P52_Q6590",
+	"74": "P52_Q6591",
+	"75": "P52_Q6592",
+	"76": "P52_Q6593",
+	"77": "P52_Q6594",
+	"78": "P52_Q6595",
+	"79": "P52_Q6596",
+	"8": "P52_Q6597",
+	"81": "P52_Q6598",
+	"82": "P52_Q6599",
+	"9": "P52_Q6600",
+	"90": "P52_Q6601",
+	"91": "P52_Q6602",
+	"92": "P52_Q6603",
+	"93": "P52_Q6604"
+}
 }
 
 
 def getingumagroup(groupname):
-	result = []
-	page = -1
+	result = {}
+	page = 0
 	r = ""
-	while page == -1 or len(r.json()) == 250:
+	while page == 0 or len(r.json()) == 250:
 		page += 1
 
 		r = ""
@@ -36,10 +153,15 @@ def getingumagroup(groupname):
 			print('Length of this slice: '+str(len(r.json())))
 			#print(r.json())
 			time.sleep(1)
-		result += r.json()
+		for entry in r.json():
+			result[entry['id']] = entry
 
 	with open('D:/Inguma/'+groupname+'.json', 'w') as jsonfile:
 		json.dump(result, jsonfile, indent=2)
 	return(result)
 
-#result = getingumagroup('persons')
+# result = getingumagroup('persons')
+# result = getingumagroup('productions')
+# result = getingumagroup('affiliations')
+# result = getingumagroup('knowledge-areas')
+# result = getingumagroup('organizations')
