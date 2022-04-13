4213869ad2e249e0d9d8fb0cc783d2238189e430
David Lindemann <34068491+dlindem@users.noreply.github.com>
2022-04-13 23:13:29 +0200

    Update inguma.py

--- inguma/inguma.py
+++ inguma/inguma.py
@@ -23,7 +23,7 @@ mappings = {
 },
 "productions":
 {
-	"classqid": "Q3",
+	"classqid": "Q8",
 	"id": "extra:",
     "title": "mon:eu:P10",
     "cleanTitle": "str:P12",
@@ -31,7 +31,7 @@ mappings = {
     "year": "extra:",
     "shu": "item:UDC",
     "url": "url:P24",
-    "organizationId": "extra:",
+    "organizationId": "", # this is done in get_orgs
     "issue": "str:P26",
     "firstPage": "str:P27",
     "lastPage": "str:P28",
@@ -51,9 +51,23 @@ mappings = {
     "issn": "extra:",
     "cleanName": "str:P11",
 },
-"item:ingumaProdType":
+"knowledge-areas":
 {
-
+	"classqid":"Q11",
+	"id": "extra:",
+	"knowledgeArea": "" # add_labels will use this
+},
+"item:ingumaProdType": # Inguma records of type value "None" here are (for the moment) ignored
+{
+	'phd': "P58_Q13",
+	'article': "P58_Q9",
+	'course': None,
+	'introduction': None,
+	'book': "P58_Q10",
+	'translation': None,
+	'conference': None,
+	'subject': None,
+	'research': None
 },
 "item:ingumaOrgType": # this is translated to class Q6 (org) or class Q7 (journal)
 {
@@ -64,81 +78,124 @@ mappings = {
 },
 "item:UDC":
 {
-	"0": "P52_Q6535",
-	"01": "P52_Q6536",
-	"02": "P52_Q6537",
-	"03": "P52_Q6538",
-	"05": "P52_Q6539",
-	"06": "P52_Q6540",
-	"07": "P52_Q6541",
-	"08": "P52_Q6542",
-	"09": "P52_Q6543",
-	"1": "P52_Q6544",
-	"11": "P52_Q6545",
-	"13": "P52_Q6546",
-	"14": "P52_Q6547",
-	"15": "P52_Q6548",
-	"16": "P52_Q6549",
-	"17": "P52_Q6550",
-	"2": "P52_Q6551",
-	"3": "P52_Q6552",
-	"30": "P52_Q6553",
-	"31": "P52_Q6554",
-	"32": "P52_Q6555",
-	"33": "P52_Q6556",
-	"34": "P52_Q6557",
-	"35": "P52_Q6558",
-	"36": "P52_Q6559",
-	"37": "P52_Q6560",
-	"39": "P52_Q6561",
-	"4": "P52_Q6562",
-	"41": "P52_Q6563",
-	"42": "P52_Q6564",
-	"43": "P52_Q6565",
-	"44": "P52_Q6566",
-	"5": "P52_Q6567",
-	"51": "P52_Q6568",
-	"52": "P52_Q6569",
-	"53": "P52_Q6570",
-	"54": "P52_Q6571",
-	"55": "P52_Q6572",
-	"56": "P52_Q6573",
-	"57": "P52_Q6574",
-	"58": "P52_Q6575",
-	"59": "P52_Q6576",
-	"6": "P52_Q6577",
-	"61": "P52_Q6578",
-	"62": "P52_Q6579",
-	"63": "P52_Q6580",
-	"64": "P52_Q6581",
-	"65": "P52_Q6582",
-	"66": "P52_Q6583",
-	"67": "P52_Q6584",
-	"68": "P52_Q6585",
-	"69": "P52_Q6586",
-	"7": "P52_Q6587",
-	"71": "P52_Q6588",
-	"72": "P52_Q6589",
-	"73": "P52_Q6590",
-	"74": "P52_Q6591",
-	"75": "P52_Q6592",
-	"76": "P52_Q6593",
-	"77": "P52_Q6594",
-	"78": "P52_Q6595",
-	"79": "P52_Q6596",
-	"8": "P52_Q6597",
-	"81": "P52_Q6598",
-	"82": "P52_Q6599",
-	"9": "P52_Q6600",
-	"90": "P52_Q6601",
-	"91": "P52_Q6602",
-	"92": "P52_Q6603",
-	"93": "P52_Q6604"
+	"0": "P53_Q6535",
+	"01": "P53_Q6536",
+	"02": "P53_Q6537",
+	"03": "P53_Q6538",
+	"05": "P53_Q6539",
+	"06": "P53_Q6540",
+	"07": "P53_Q6541",
+	"08": "P53_Q6542",
+	"09": "P53_Q6543",
+	"1": "P53_Q6544",
+	"11": "P53_Q6545",
+	"13": "P53_Q6546",
+	"14": "P53_Q6547",
+	"15": "P53_Q6548",
+	"16": "P53_Q6549",
+	"17": "P53_Q6550",
+	"2": "P53_Q6551",
+	"3": "P53_Q6552",
+	"30": "P53_Q6553",
+	"31": "P53_Q6554",
+	"32": "P53_Q6555",
+	"33": "P53_Q6556",
+	"34": "P53_Q6557",
+	"35": "P53_Q6558",
+	"36": "P53_Q6559",
+	"37": "P53_Q6560",
+	"39": "P53_Q6561",
+	"4": "P53_Q6562",
+	"41": "P53_Q6563",
+	"42": "P53_Q6564",
+	"43": "P53_Q6565",
+	"44": "P53_Q6566",
+	"5": "P53_Q6567",
+	"51": "P53_Q6568",
+	"52": "P53_Q6569",
+	"53": "P53_Q6570",
+	"54": "P53_Q6571",
+	"55": "P53_Q6572",
+	"56": "P53_Q6573",
+	"57": "P53_Q6574",
+	"58": "P53_Q6575",
+	"59": "P53_Q6576",
+	"6": "P53_Q6577",
+	"61": "P53_Q6578",
+	"62": "P53_Q6579",
+	"63": "P53_Q6580",
+	"64": "P53_Q6581",
+	"65": "P53_Q6582",
+	"66": "P53_Q6583",
+	"67": "P53_Q6584",
+	"68": "P53_Q6585",
+	"69": "P53_Q6586",
+	"7": "P53_Q6587",
+	"71": "P53_Q6588",
+	"72": "P53_Q6589",
+	"73": "P53_Q6590",
+	"74": "P53_Q6591",
+	"75": "P53_Q6592",
+	"76": "P53_Q6593",
+	"77": "P53_Q6594",
+	"78": "P53_Q6595",
+	"79": "P53_Q6596",
+	"8": "P53_Q6597",
+	"81": "P53_Q6598",
+	"82": "P53_Q6599",
+	"9": "P53_Q6600",
+	"90": "P53_Q6601",
+	"91": "P53_Q6602",
+	"92": "P53_Q6603",
+	"93": "P53_Q6604"
 }
 }
 
+def get_production_authors(prod_id):
+	r = ""
+	while '200' not in str(r):
+		print('Now downloading author info for production '+str(prod_id))
+		r = requests.get('https://www.inguma.eus/rest/production-persons?search={"productionId":'+str(prod_id)+'}', auth=requests.auth.HTTPBasicAuth(config_private.inguma_api_user,config_private.inguma_api_pwd))
+		#print(str(r))
 
-def getingumagroup(groupname):
+		#print(r.json())
+		#time.sleep(1)
+		authors = {}
+		try:
+			print('Number of authors: '+str(len(r.json())))
+			for index in range(len(r.json())):
+				authors[str(index+1)] = str(r.json()[index]['personId'])
+			return authors
+		except:
+			if '204'in str(r):
+				print('Response "No content".')
+				return {}
+			print('Error downloading authors. Will retry in 2 sec...')
+			time.sleep(2)
+
+def get_production_knowlareas(prod_id):
+	r = ""
+	while '200' not in str(r):
+		print('Now downloading knowledge area info for production '+str(prod_id))
+		r = requests.get('https://www.inguma.eus/rest/knowledge-area-productions?search={"productionId":'+str(prod_id)+'}', auth=requests.auth.HTTPBasicAuth(config_private.inguma_api_user,config_private.inguma_api_pwd))
+		#print(str(r))
+
+		#print(r.json())
+		#time.sleep(1)
+		areas = []
+		try:
+			print('Number of knowledge areas: '+str(len(r.json())))
+			for index in r.json():
+				areas.append(index['knowledgeAreaId'])
+			return areas
+		except:
+			if '204'in str(r):
+				print('Response "No content".')
+				return []
+			print('Error downloading areas. Will retry in 2 sec...')
+			time.sleep(2)
+
+def get_ingumagroup(groupname):
 	result = {}
 	page = 0
 	r = ""
@@ -149,19 +206,19 @@ def getingumagroup(groupname):
 		while '200' not in str(r):
 			print('Now downloading slice '+str(page)+'...')
 			r = requests.get('https://www.inguma.eus/rest/'+groupname+'?limit=250&page='+str(page), auth=requests.auth.HTTPBasicAuth(config_private.inguma_api_user,config_private.inguma_api_pwd))
-			print(str(r))
+			# print(str(r))
 			print('Length of this slice: '+str(len(r.json())))
 			#print(r.json())
 			time.sleep(1)
 		for entry in r.json():
 			result[entry['id']] = entry
 
-	with open('D:/Inguma/'+groupname+'.json', 'w') as jsonfile:
+	with open('D:/Inguma/content/'+groupname+'.json', 'w') as jsonfile:
 		json.dump(result, jsonfile, indent=2)
 	return(result)
 
-# result = getingumagroup('persons')
-# result = getingumagroup('productions')
-# result = getingumagroup('affiliations')
-# result = getingumagroup('knowledge-areas')
-# result = getingumagroup('organizations')
+# result = get_ingumagroup('persons')
+# result = get_ingumagroup('productions')
+# result = get_ingumagroup('affiliations')
+# result = get_ingumagroup('knowledge-areas')
+# result = get_ingumagroup('organizations')
