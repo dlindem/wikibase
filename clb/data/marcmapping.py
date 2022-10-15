2fe3f0d624fe3051a3255447150d00deb984dc08
David Lindemann <34068491+dlindem@users.noreply.github.com>
2022-10-15 14:13:59 +0200

    Create marcmapping.py

---
+++ clb/data/marcmapping.py
@@ -0,0 +1,14 @@
+mapping = {
+    "151": [{'target': 'cslex', 'value':{'sub': 'a', 'ind1': ' ', 'ind2': ' '}}],
+    "751": [{'target': 'enlex', 'value':{'sub': 'a', 'ind1': '0', 'ind2': '7'}}],
+    "034": [{'target': 'geo_ew', 'value':{'sub': 'd', 'ind1': ' ', 'ind2': ' '}}, {'target': 'geo_ns', 'value':{'sub': 'f', 'ind1': ' ', 'ind2': ' '}}],
+    "080": [{'target': 'udc', 'value':{'sub': 'a', 'ind1': ' ', 'ind2': ' '}}],
+    "150": [{'target': 'cslex', 'value':{'sub': 'a', 'ind1': ' ', 'ind2': ' '}}],
+    "450": [{'target': 'csaltlex', 'value':{'sub': 'a', 'ind1': ' ', 'ind2': ' '}}],
+    "750": [{'target': 'enlex', 'value':{'sub': 'a', 'ind1': '0', 'ind2': '7'}}],
+    "550": [{'target': 'broader', 'condition':{'sub':'w', 'value':'g'}, 'value':{'sub': '7', 'ind1': ' ', 'ind2': ' '}},
+            {'target': 'narrower', 'condition':{'sub':'w', 'value':'h'}, 'value':{'sub': '7', 'ind1': ' ', 'ind2': ' '}},
+            {'target': 'related', 'condition':{'nosub':'w'}, 'value':{'sub': '7', 'ind1': ' ', 'ind2': ' '}}],
+    "080": [{'target': 'udc', 'value':{'sub': 'a', 'ind1': ' ', 'ind2': ' '}}]
+
+}
