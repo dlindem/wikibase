0a5a24b1185c145945f3c8f7bec270099e055b8e
David Lindemann <34068491+dlindem@users.noreply.github.com>
2022-10-15 14:40:51 +0200

    Update marcmapping.py

--- clb/data/marcmapping.py
+++ clb/data/marcmapping.py
@@ -9,6 +9,6 @@ mapping = {
     "550": [{'target': 'broader', 'condition':{'sub':'w', 'value':'g'}, 'value':{'sub': '7', 'ind1': ' ', 'ind2': ' '}},
             {'target': 'narrower', 'condition':{'sub':'w', 'value':'h'}, 'value':{'sub': '7', 'ind1': ' ', 'ind2': ' '}},
             {'target': 'related', 'condition':{'nosub':'w'}, 'value':{'sub': '7', 'ind1': ' ', 'ind2': ' '}}],
-    "080": [{'target': 'udc', 'value':{'sub': 'a', 'ind1': ' ', 'ind2': ' '}}]
-
+    "155": [{'target': 'cslex', 'value':{'sub': 'a', 'ind1': ' ', 'ind2': ' '}}],
+    "755": [{'target': 'enlex', 'value':{'sub': 'a', 'ind1': '0', 'ind2': '7'}}]
 }
