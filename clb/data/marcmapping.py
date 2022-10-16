8f3167ebd751011dcc4ff9c21f56d9104240439c
David Lindemann <34068491+dlindem@users.noreply.github.com>
2022-10-16 19:26:44 +0200

    add docstrings

--- clb/data/marcmapping.py
+++ clb/data/marcmapping.py
@@ -12,3 +12,13 @@ mapping = {
     "155": [{'target': 'cslex', 'value':{'sub': 'a', 'ind1': ' ', 'ind2': ' '}}],
     "755": [{'target': 'enlex', 'value':{'sub': 'a', 'ind1': '0', 'ind2': '7'}}]
 }
+
+# These are the MARC mapping rules, valid for CLB MARC data.
+# dictionary key is the MARC field code ("tag" attribute of controlfield and datafield elements)
+
+# dictionarie value contains a list of actions, each of which contains:
+# 'target' (mandatory): can be mapped to wikibase properties, or used to trigger a specially defined process
+# 'value' (mandatory): indicates indicator values and subfield code where the value to write can be found
+# 'condition' (optional): a dict with either 'sub' or 'nosub' as key:
+# 'sub': subfield with code must contain value for action to be valid (e.g. for 550 'narrower term' / 'broader term')
+# 'nosub': subfield with code must not exist for action to be valid (e.g. for 550 'related term')
