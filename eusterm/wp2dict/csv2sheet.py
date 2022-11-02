1d2e669183b0dc6b2524e2a7724714b15d89b7b7
David Lindemann <34068491+dlindem@users.noreply.github.com>
2022-11-02 17:51:16 +0100

    move to wikibase/eusterm

---
+++ eusterm/wp2dict/csv2sheet.py
@@ -0,0 +1,17 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def csv2sheet(title=None, csvpath=None):
+
+	scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+			 "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+	credentials = ServiceAccountCredentials.from_json_keyfile_name('awesome-sphere-367416-3a07f9137ac1.json', scope)
+	gc = gspread.authorize(credentials)
+
+	spreadsheet = gc.create(title)
+	spreadsheet.share('david.lindemann.soraluze@gmail.com', perm_type='user', role='writer')
+
+	with open('csvpath', 'r') as file_obj:
+		csv_content = file_obj.read()
+		gc.import_csv(spreadsheet.id, data=csv_content)
