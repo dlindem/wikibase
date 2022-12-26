c7f03b4c07165b247ef19288971a5684fe1a1071
David Lindemann <34068491+dlindem@users.noreply.github.com>
2022-12-26 10:09:22 +0100

    Include 404 exception handling

--- kurdish/lexeme-upload.py
+++ kurdish/lexeme-upload.py
@@ -1,4 +1,4 @@
-import kwbi, csv, json, sys
+import kwbi, csv, json, sys, time
 from data import langmapping
 
 with open('data/lexeme-mapping.csv') as mappingcsv:
@@ -11,37 +11,49 @@ with open('data/lexeme-mapping.csv') as mappingcsv:
 
 with open('data/lexical-entry.csv') as incsv:
 	entries = csv.DictReader(incsv)
-	with open('data/lexeme-mapping.csv', 'a') as mappingcsv:
-		count = 0
 
-		for entry in entries:
-			count += 1
-			sinaid = entry['entry'].replace('https://github.com/sinaahmadi/', '')
-			if sinaid in lexeme_map:
-				continue # has been processed in previous run
-			print('\nWill process entry file line '+str(count)+': '+sinaid)
+	count = 0
+	for entry in entries:
+		count += 1
+		sinaid = entry['entry'].replace('https://github.com/sinaahmadi/', '')
+		if sinaid in lexeme_map:
+			continue # has been processed in previous run
+		print('\nWill process entry file line '+str(count)+': '+sinaid)
 
-			lemmas = {}
-			for label in json.loads(entry['labels']):
-				languages = []
-				labellang = label['lang'].lower()
-				if labellang == "sdh-arab":
-					continue # sdh-arab is skipped (for the moment)
-				if labellang == "sdh-latn":
-					labellang = "sdh" # sdh-latn is represented as sdh
-				languages.append(labellang.replace('-arab','').replace('-latn',''))
-				lemmas[labellang] = label['label']
-				if len(list(set(languages))) > 1:
-					print('Error: more than one lemma language')
+		lemmas = {}
+		for label in json.loads(entry['labels']):
+			languages = []
+			labellang = label['lang'].lower()
+			if labellang == "sdh-arab":
+				continue # sdh-arab is skipped (for the moment)
+			if labellang == "sdh-latn":
+				labellang = "sdh" # sdh-latn is represented as sdh
+			languages.append(labellang.replace('-arab','').replace('-latn',''))
+			lemmas[labellang] = label['label']
+			if len(list(set(languages))) > 1:
+				print('Error: more than one lemma language')
+				sys.exit()
+
+		lexeme = kwbi.wbi.lexeme.new(lexical_category="Q3", language=langmapping.wikilang_to_qid[languages[0]])
+		for labellang in lemmas:
+			lexeme.lemmas.set(language=labellang, value=lemmas[labellang])
+
+		claim = kwbi.String(prop_nr='P6', value=sinaid)
+		lexeme.claims.add(claim)
+
+		done = False
+		while not done:
+			try:
+				lexeme.write()
+				done = True
+			except Exception as ex:
+				if "404 Client Error" in str(ex):
+					print('Got 404 response from wikibase, will wait and try again...')
+					time.sleep(10)
+				else:
+					print('Unexpected error:\n'+str(ex))
 					sys.exit()
 
-			lexeme = kwbi.wbi.lexeme.new(lexical_category="Q3", language=langmapping.wikilang_to_qid[languages[0]])
-			for labellang in lemmas:
-				lexeme.lemmas.set(language=labellang, value=lemmas[labellang])
-			
-			claim = kwbi.String(prop_nr='P6', value=sinaid)
-			lexeme.claims.add(claim)
-
-			lexeme.write()
+		with open('data/lexeme-mapping.csv', 'a') as mappingcsv:
 			mappingcsv.write(sinaid+'\t'+lexeme.id+'\n')
-			print('Finished processing '+lexeme.id)
+		print('Finished processing '+lexeme.id)
