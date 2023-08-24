1f8c37f1bf901febb9b58161559eda4d86b0c0dc
David Lindemann <34068491+dlindem@users.noreply.github.com>
2023-08-25 00:46:58 +0200

    Update zoterobot.py

--- lexbib/zoterobot.py
+++ lexbib/zoterobot.py
@@ -1,7 +1,19 @@
-import requests, time, re, config
+import requests, time, re, json, config
 from config_private import zotero_group_id, zotero_api_key
 
+citations_cache = {}
+with open('data/citations_cache.jsonl') as jsonlfile:
+	jsonl = jsonlfile.read().split('\n')
+	for line in jsonl:
+		if line.startswith("{"):
+			jsonline = json.loads(line)
+			citations_cache[jsonline['zotitemid']] = jsonline['citation']
+
 def getcitation(zotitemid):
+	global citations_cache
+	if zotitemid in citations_cache:
+		print(f"Will take citation from cache: {zotitemid}")
+		return citations_cache[zotitemid]
 	print(f'Will now get citation for Zotero ID {zotitemid}')
 	zotapid = 'https://api.zotero.org/groups/'+zotero_group_id+'/items/'+zotitemid
 	attempts = 0
@@ -14,11 +26,14 @@ def getcitation(zotitemid):
 			# print(zotitemid + ': got zotitem data')
 			# print(zotitem['bib'])
 			bib = re.search('<div class="csl-entry">(.*)</div>', zotitem['bib']).group(1)
-			# de-html links
-			bib = re.sub('<a href=[^>]+>', '', bib)
-			bib = re.sub('</a>', '', bib)
 			# convert Lexbib link (from "archive location" - needs mla style)
 			bib = re.sub(r'https?://lexbib.elex.is/entity/(Q[0-9]+)', r'([[Item:\1|\1]])', bib)
+			# convert remaining links
+			bib = re.sub(r'<a href="(https?://)([^/]+)([^"]+)">[^<]+</a>', r'[\1\2\3 \2]', bib)
+			print(bib)
+			citations_cache[zotitemid] = bib
+			with open('data/citations_cache.jsonl', 'a', encoding='utf-8') as jsonlfile:
+				jsonlfile.write(json.dumps({'zotitemid':zotitemid,'citation':bib})+'\n')
 			return(bib)
 
 		if "400" or "404" in str(r):
