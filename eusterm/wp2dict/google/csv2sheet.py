eaec9b6c410099d76bd523cb76f195be34f6dca9
David Lindemann <34068491+dlindem@users.noreply.github.com>
2023-10-18 23:38:57 +0200

    Move to extra folder

---
+++ eusterm/wp2dict/google/csv2sheet.py
@@ -0,0 +1,20 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def csv2sheet(title=None, csvpath=None):
+
+	scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+			 "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+	credentials = ServiceAccountCredentials.from_json_keyfile_name('google/awesome-sphere-367416-3a07f9137ac1.json', scope)
+	gc = gspread.authorize(credentials)
+
+	spreadsheet = gc.create(title)
+	spreadsheet.share('david.lindemann.soraluze@gmail.com', perm_type='user', role='writer')
+	print('Created and shared google sheet: '+title)
+	with open(csvpath, 'r', encoding="utf-8") as file_obj:
+		csv_content = file_obj.read()
+		gc.import_csv(spreadsheet.id, data=csv_content)
+		print('Successfully imported csv into google sheet.')
+
+# csv2sheet(title="wp2dict_es_Nutricion", csvpath="output/Nutrici%C3%B3n.es.csv")
