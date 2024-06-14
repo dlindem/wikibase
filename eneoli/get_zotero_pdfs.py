c9740802f47bef52d3ed31a570d2a042e19f5033
David Lindemann <34068491+dlindem@users.noreply.github.com>
2024-06-15 01:42:19 +0200

    Update get_zotero_pdfs.py

--- eneoli/get_zotero_pdfs.py
+++ eneoli/get_zotero_pdfs.py
@@ -1,4 +1,7 @@
 import os, csv
+import config_private
+from pyzotero import zotero
+pyzot = zotero.Zotero(config_private.zotero_group_nr, 'group', config_private.zotero_api_key)
 
 # PREFIX enwb: <https://eneoli.wikibase.cloud/entity/>
 # PREFIX endp: <https://eneoli.wikibase.cloud/prop/direct/>
@@ -6,15 +9,16 @@ import os, csv
 # PREFIX enps: <https://eneoli.wikibase.cloud/prop/statement/>
 # PREFIX enpq: <https://eneoli.wikibase.cloud/prop/qualifier/>
 #
-# select ?item_id ?parent_id ?pdf_id ?iso
+# select ?item_id ?parent_id ?pdf_id ?iso (sample(?desc) as ?description)
 #
 # where {
 #   ?item endp:P5 enwb:Q2; enp:P17 ?zotst. ?zotst enps:P17 ?parent_id; enpq:P20 ?pdf_id.
 #   ?item endp:P7 ?lang. ?lang endp:P50 ?iso. filter(?iso="fra") # lang fr
-#   ?item endp:P6 enwb:Q9. # type journal article
+#   # ?item endp:P6 enwb:Q9. # type journal article
+#   ?item schema:description ?desc.
 #   bind(strafter(str(?item),str(enwb:)) as ?item_id)
 #
-# } order by ?iso
+# } group by ?item_id ?parent_id ?pdf_id ?iso ?description order by ?iso
 
 with open('data/fulltext_items.csv') as file:
     rows = csv.DictReader(file)
@@ -25,8 +29,14 @@ with open('data/fulltext_items.csv') as file:
         bibitem_qid = row['item_id']
         lang_iso = row['iso']
         try:
-            os.system(f"cp /home/david/Zotero/storage/{pdf_id}/*.pdf /media/david/FATdisk/ENEOLI/eneoli_fulltext_{bibitem_qid}.pdf")
-            print(f"Successfully copied {pdf_id}.")
+            source = f"/home/david/Zotero/storage/{pdf_id}"
+            if not os.path.exists(source):
+                zotitem = pyzot.item(parent_id)
+                zotitem = pyzot.add_tags(zotitem, 'wikibase-export')
+                print(f"Tagged item {parent_id} with 'wikibase-export', there seems to be an orphaned PDF ID qualifier in the zotitem ID statement.")
+            else:
+                os.system(f"cp {source}/*.pdf /media/david/FATdisk/ENEOLI/eneoli_fulltext_{bibitem_qid}.pdf")
+                print(f"Successfully copied {pdf_id}.")
         except:
             print(f"There is something wrong with {row}...")
 
