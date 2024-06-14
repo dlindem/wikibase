5056fbd52b9acc99a596f2be12d13dc78ae1bd4f
David Lindemann <34068491+dlindem@users.noreply.github.com>
2024-06-15 01:42:16 +0200

    Update process_tei.py

--- eneoli/process_tei.py
+++ eneoli/process_tei.py
@@ -4,8 +4,8 @@ import config_private
 from pyzotero import zotero
 pyzot = zotero.Zotero(config_private.zotero_group_nr, 'group', config_private.zotero_api_key)
 
-overwrite_existing_json = True
-
+overwrite_existing_json = False
+missing_tei_list = []
 
 with open('data/fulltext_items.csv') as file:
     rows = csv.DictReader(file)
@@ -15,6 +15,10 @@ with open('data/fulltext_items.csv') as file:
         pdf_id = row['pdf_id']
         bibitem_qid = row['item_id']
         lang_iso = row['iso']
+        desctext = re.sub(r'[^\w\-_]', '' ,row['description'].strip().replace(" ","_"))
+        xmlfile = f"/media/david/FATdisk/ENEOLI/eneoli_fulltext_{bibitem_qid}.tei.xml"
+        resultfile = f"/media/david/FATdisk/ENEOLI/eneoli_fulltext_{bibitem_qid}.json"
+
         print(f"\nNow processing {bibitem_qid}, Zotero {parent_id}, language is {lang_iso}.")
         zotitem = pyzot.item(parent_id)
         zotchildren = pyzot.children(parent_id)
@@ -41,6 +45,8 @@ with open('data/fulltext_items.csv') as file:
                         print(f'Found duplicate {filext}."')
                         if pdf_id == attkey:
                             print(f"Found the pdf referenced on Wikibase: {pdf_id}")
+                            delete_op = pyzot.delete_item(pyzot.item(control['.pdf']))
+                            print(f"Have deleted this repeated {filext} which raised the problem before: {control['.pdf']}.")
                             control['.pdf'] = pdf_id
                         elif filext != ".txt": # do not delete extra txt, just stay with the first one
                             try:
@@ -53,7 +59,7 @@ with open('data/fulltext_items.csv') as file:
         print(control)
         if control['.json'] and not overwrite_existing_json:
             print('Will skip item with existing full text json file.')
-            continue
+
         elif control['.json'] and overwrite_existing_json:
             try:
                 delete_op = pyzot.delete_item(pyzot.item(control['.json']))
@@ -62,48 +68,63 @@ with open('data/fulltext_items.csv') as file:
                 print(f"ERROR: No success deleting full text json file {control['json']}!!")
                 time.sleep(1)
 
-        xmlfile = f"/media/david/FATdisk/ENEOLI/eneoli_fulltext_{bibitem_qid}.tei.xml"
-        resultfile = f"/media/david/FATdisk/ENEOLI/eneoli_fulltext_{bibitem_qid}.json"
-
-        if control['.txt']:
-            for file in os.listdir(f"/home/david/Zotero/storage/{control['.txt']}"):
-                if file.endswith(".txt"):
-                    txtfile = f"/home/david/Zotero/storage/{control['.txt']}/{file}"
-                    with open(txtfile, encoding="utf-8") as file:
-                        bodytxt = nlp.check_encoding(file.read(), control['.txt'])
-                    teijson = {'abstract': '', 'body': bodytxt}
-                    print('This item has TXT, have taken text from there as body.')
-
         else:
 
-            if not os.path.isfile(xmlfile):
-                print(f"There is something wrong with {xmlfile}.")
-                if not control['.txt']:
-                    continue
+            if control['.txt']:
+                for file in os.listdir(f"/home/david/Zotero/storage/{control['.txt']}"):
+                    if file.endswith(".txt"):
+                        txtfile = f"/home/david/Zotero/storage/{control['.txt']}/{file}"
+                        with open(txtfile, encoding="utf-8") as file:
+                            bodytxt = nlp.check_encoding(file.read(), control['.txt'])
+                        teijson = {'abstract': '', 'body': bodytxt}
+                        print('This item has TXT, have taken text from there as body.')
+
             else:
-                if control['.xml'] == None:
-                    pyzot.attachment_simple([xmlfile], parentid=parent_id)
-                    print(f"Success attaching {xmlfile} to Zotero {parent_id}...")
-            teijson = nlp.getgrobidabstractbody(xmlfile)
 
-        teijson['abstract_lemmatized'] = nlp.lemmatize_clean(teijson['abstract'], lang=lang_iso)
-        teijson['body_lemmatized'] = nlp.lemmatize_clean(teijson['body'], lang=lang_iso)
+                if not os.path.isfile(xmlfile):
+                    print(f"Did not find {xmlfile}.")
+                    missing_tei_list.append(bibitem_qid)
+                    zotitem = pyzot.add_tags(zotitem, 'failed_TEI')
+                    print(f"Added 'failed_TEI' tag to {parent_id} ({bibitem_qid}).")
+                    if not control['.txt']:
+                        continue
+                else:
+                    if control['.xml'] == None:
+                        pyzot.attachment_simple([xmlfile], parentid=parent_id)
+                        print(f"Success attaching {xmlfile} to Zotero {parent_id}...")
+                teijson = nlp.getgrobidabstractbody(xmlfile)
 
-        with open(resultfile, 'w') as jsonfile:
-            json.dump(dict(sorted(teijson.items())), jsonfile, indent=2)
-        print(f"Success processing and saving {resultfile}...")
+            teijson['abstract_lemmatized'] = nlp.lemmatize_clean(teijson['abstract'], lang=lang_iso)
+            teijson['body_lemmatized'] = nlp.lemmatize_clean(teijson['body'], lang=lang_iso)
 
-
-
-        pyzot.attachment_simple([resultfile], parentid=parent_id)
-        print(f"Success attaching {resultfile} to Zotero item {parent_id}...")
-
-        try:
-            pyzot.add_tags(zotitem, 'processed_fulltext')
-            print(f"Success tagging Zotero item {parent_id}...")
-        except:
-            print(f"ERROR: No success tagging Zotero item {parent_id}!!")
-
-        time.sleep(1)
+            with open(resultfile, 'w') as jsonfile:
+                json.dump(dict(sorted(teijson.items())), jsonfile, indent=2)
+            print(f"Success processing and saving {resultfile}...")
 
 
+
+            pyzot.attachment_simple([resultfile], parentid=parent_id)
+            print(f"Success attaching {resultfile} to Zotero item {parent_id}...")
+
+            try:
+                pyzot.add_tags(zotitem, 'processed_fulltext')
+                print(f"Success tagging Zotero item {parent_id}...")
+            except:
+                print(f"ERROR: No success tagging Zotero item {parent_id}!!")
+
+        # last step: a body txt for Sketch Engine
+        ske_filename = f"/media/david/FATdisk/ENEOLI/NeoCorpus_TXT_for_SkE/{lang_iso}/{bibitem_qid}_{desctext}.txt"
+        if not os.path.exists(ske_filename):
+            with open(resultfile) as jsonfile:
+                teijson = json.load(jsonfile)
+                ske_txt = f"{teijson['abstract']} {teijson['body']}"
+            with open(ske_filename, 'w') as txtfile:
+                txtfile.write(ske_txt)
+            print(f"Successfully stored TXT for SkE.")
+        else:
+            print(f"SkE TXT file is there, no need to produce it.")
+
+        time.sleep(0.5)
+
+with open('data/missing_tei_list.json', 'w') as jsonfile:
+    json.dump(missing_tei_list, jsonfile)
