c95dba0aec37fed7bc668e81ce899d2e80ca07de
David Lindemann <34068491+dlindem@users.noreply.github.com>
2024-07-15 13:22:36 +0200

    2nd batch of French terms

--- eneoli/config.py
+++ eneoli/config.py
@@ -2,4 +2,5 @@ api_url = "https://eneoli.wikibase.cloud/w/api.php"
 sparql_endpoint = "https://eneoli.wikibase.cloud/query/sparql"
 wikibase_url = "https://eneoli.wikibase.cloud"
 wikibase_entity_ns = "https://eneoli.wikibase.cloud/entity/"
+wikibase_name = "ENEOLI"
 card1props = ['P57', 'P59', 'P12']
