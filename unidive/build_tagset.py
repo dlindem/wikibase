43c10111feecc68531229dae9765e2bbc6dc36db
David Lindemann <34068491+dlindem@users.noreply.github.com>
2024-11-13 23:24:47 +0100

    shorten sense descs to 80 char

--- unidive/build_tagset.py
+++ unidive/build_tagset.py
@@ -1,4 +1,4 @@
-import csv, json, datetime
+import csv, json, datetime, re
 
 with open('senses.csv') as file:
     reader = csv.DictReader(file)
@@ -13,7 +13,12 @@ result = {"name": "UniDive Wikibase Senses",
           "tags": []}
 
 for sense in senses:
-    result['tags'].append({'tag_name': sense['label'], 'tag_description': sense['sense']})
+    label_re = re.search(r'(.*) (\[L[^\]]+\])', sense['label'])
+    label = label_re.group(1)
+    sense_id = label_re.group(2)
+    if len(label) > 77:
+        label = sense['label'][0:77] + '...'
+    result['tags'].append({'tag_name': label +' '+ sense_id, 'tag_description': sense['sense']})
 
 with open('senses_tagset.json', 'w') as file:
     json.dump(result, file, indent=2)
