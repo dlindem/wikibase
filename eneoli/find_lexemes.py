73ee4fd5d6376ab7f54d3548acf5afc3f0300c59
David Lindemann <34068491+dlindem@users.noreply.github.com>
2025-01-23 13:08:17 +0100

    Update find_lexemes.py

--- eneoli/find_lexemes.py
+++ eneoli/find_lexemes.py
@@ -2,9 +2,12 @@ import json, re, time
 import config_private
 import xwbi
 from flashtext import KeywordProcessor
-keyword_processor = KeywordProcessor()
+
+print("Will get Zotero items...")
 from pyzotero import zotero
 pyzot = zotero.Zotero(config_private.zotero_group_nr, 'group', config_private.zotero_api_key)
+zotitems = pyzot.everything(pyzot.items(tag="on-wikibase"))
+
 
 query = """
 		PREFIX enwb: <https://eneoli.wikibase.cloud/entity/>
@@ -17,70 +20,75 @@ where {
  
 }
 		"""
-bindings = xwbi.wbi_helpers.execute_sparql_query(query=query)['results']['bindings']
-print('Found ' + str(len(bindings)) + ' ENEOLI lexical entries.\n')
+lexicon = xwbi.wbi_helpers.execute_sparql_query(query=query)['results']['bindings']
+print('Found ' + str(len(lexicon)) + ' ENEOLI lexical entries.\n')
 time.sleep(1)
 
-for binding in bindings:
-    if binding['lang']['value'] == "fr":
-        keyword_processor.add_keywords_from_dict(
-            {binding['lexical_entry']['value'].replace("https://eneoli.wikibase.cloud/entity/",""): [binding['lemma']['value']]}
-        )
-print(f"Fed keyword processor with {len(keyword_processor)} lemmata.")
+def find_lexemes_for_lang(lang):
+    global lexicon
+    global zotitems
+    keyword_processor = KeywordProcessor()
+    for binding in lexicon:
+        if binding['lang']['value'] == lang:
+            keyword_processor.add_keywords_from_dict(
+                {binding['lexical_entry']['value'].replace("https://eneoli.wikibase.cloud/entity/",""): [binding['lemma']['value']]}
+            )
+    print(f"Fed keyword processor with {len(keyword_processor)} lemmata for language '{lang}'.")
 
-zotitems = pyzot.everything(pyzot.items(tag="falta"))
-count = 0
-for zotitem in zotitems:
-    count += 1
-    children = pyzot.children(zotitem['key'])
-    for child in children:
-        if 'enclosure' in child['links']:
-            if child['links']['enclosure']['type'] == "application/json":
-                bibitemqid = re.search(r'Q\d+', child['links']['enclosure']['title']).group(0)
-                print(f"\n[{count}/{len(zotitems)}]] For {bibitemqid}, retrieving fulltext json file: {child['key']}")
 
-                fulltext = pyzot.file(child['key'])
-                # print(json.dumps(fulltext, indent=2))
-                if 'lemmatized' in fulltext:
-                    text = fulltext['lemmatized']
-                elif 'body_lemmatized' in fulltext and 'abstract_lemmatized' in fulltext:
-                    text = fulltext['abstract_lemmatized'] + ' ' + fulltext['body_lemmatized']
-                else:
-                    text = None
-    if not text:
-        print(f"Could not load fulltext JSON. Cannot process {zotitem['key']}.")
-        time.sleep(1)
-        continue
-    keywords = keyword_processor.extract_keywords(text)
-    uniqkws = set(keywords)
-    keydict= {}
-    for uniqkw in uniqkws:
-        keydict[uniqkw] = keywords.count(uniqkw)
-    freq_order = sorted(keydict, key=keydict.get, reverse=True)
-    found_lexemes = {}
-    for keyw in freq_order:
-        found_lexemes[keyw] = keydict[keyw]
-    print(f"Found lexemes: {found_lexemes}")
+    count = 0
+    for zotitem in zotitems:
+        text = False
+        count += 1
+        children = pyzot.children(zotitem['key'])
+        for child in children:
+            if 'enclosure' in child['links']:
+                if child['links']['enclosure']['type'] == "application/json":
+                    bibitemqid = re.search(r'Q\d+', child['links']['enclosure']['title']).group(0)
+                    print(f"\n[{count}/{len(zotitems)}]] For {bibitemqid}, retrieving fulltext json file: {child['key']}")
 
-    statements = []
-    first_statement = True
-    for lid in found_lexemes:
-        if first_statement:
-            statements.append({'type': 'lexeme', 'prop_nr': 'P65', 'value': lid, 'qualifiers': [
-                {'type': 'string', 'prop_nr': 'P66', 'value': str(found_lexemes[lid])}
-            ], 'action': 'replace'})
-            xwbi.itemwrite({'qid': bibitemqid, 'statements': statements})
-            time.sleep(0.2)
-            first_statement = False
-        else:
-            statements.append({'type': 'lexeme', 'prop_nr': 'P65', 'value': lid, 'qualifiers':[
-                {'type': 'string', 'prop_nr': 'P66', 'value': str(found_lexemes[lid])}
-            ], 'action': 'append'})
-    # print(json.dumps(statements, indent=2))
+                    fulltext = pyzot.file(child['key'])
+                    # print(json.dumps(fulltext, indent=2))
+                    if 'lemmatized' in fulltext:
+                        text = fulltext['lemmatized']
+                    elif 'body_lemmatized' in fulltext and 'abstract_lemmatized' in fulltext:
+                        text = fulltext['abstract_lemmatized'] + ' ' + fulltext['body_lemmatized']
+                    else:
+                        text = None
+        if not text:
+            print(f"Could not load fulltext JSON. Cannot process {zotitem['key']}.")
+            time.sleep(1)
+            continue
+        keywords = keyword_processor.extract_keywords(text)
+        uniqkws = set(keywords)
+        keydict= {}
+        for uniqkw in uniqkws:
+            keydict[uniqkw] = keywords.count(uniqkw)
+        freq_order = sorted(keydict, key=keydict.get, reverse=True)
+        found_lexemes = {}
+        for keyw in freq_order:
+            found_lexemes[keyw] = keydict[keyw]
+        print(f"Found lexemes: {found_lexemes}")
 
-    xwbi.itemwrite({'qid': bibitemqid, 'statements': statements})
-    zotitem['data']['tags'].append({"tag": "term-indexed"})
-    pyzot.update_item(zotitem)
-    print(f"Added tag 'term-indexed' to {zotitem['key']}")
-    print(f"Finished processing <https://eneoli.wikibase.cloud/entity/{bibitemqid}>")
+        statements = []
+        first_statement = True
+        for lid in found_lexemes:
+            if first_statement:
+                statements.append({'type': 'lexeme', 'prop_nr': 'P65', 'value': lid, 'qualifiers': [
+                    {'type': 'string', 'prop_nr': 'P66', 'value': str(found_lexemes[lid])}
+                ], 'action': 'replace'})
+                xwbi.itemwrite({'qid': bibitemqid, 'statements': statements})
+                time.sleep(0.2)
+                first_statement = False
+            else:
+                statements.append({'type': 'lexeme', 'prop_nr': 'P65', 'value': lid, 'qualifiers':[
+                    {'type': 'string', 'prop_nr': 'P66', 'value': str(found_lexemes[lid])}
+                ], 'action': 'append'})
+        # print(json.dumps(statements, indent=2))
+
+        xwbi.itemwrite({'qid': bibitemqid, 'statements': statements})
+        zotitem['data']['tags'].append({"tag": "term-indexed"})
+        pyzot.update_item(zotitem)
+        print(f"Added tag 'term-indexed' to {zotitem['key']}")
+        print(f"Finished processing <https://eneoli.wikibase.cloud/entity/{bibitemqid}>")
 
