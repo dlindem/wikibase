bb0084f52453396d3e5d67928456ead5a44b2eef
David Lindemann <34068491+dlindem@users.noreply.github.com>
2025-01-29 15:27:44 +0100

    Create mappings.py

---
+++ tbx-convert/mappings.py
@@ -0,0 +1,38 @@
+namespaces = {"tbx3": "urn:iso:std:iso:30042:ed-2",
+              "min": "http://www.tbxinfo.net/ns/min",
+              "basic": "http://www.tbxinfo.net/ns/basic",
+              "fairterm": "http://www.fairterm.org/ns/sample",
+              }
+
+properties = {
+    "conceptEntry":
+        {"min:subjectField": "",
+         "fairterm:subDomain": "",
+         "fairterm:genericConcept": "",
+         "fairterm:specificConcept": "",
+         "fairterm:comprehensiveConcept": "",
+         "fairterm:partitiveConcept": ""
+         },
+    "termSec":
+        {"fairterm:idenitifer": "",
+         "fairterm:usage": "",
+         "min:partOfSpeech": "",
+         "basic:grammaticalGender": "",
+         "basic:termType": "",
+         "fairterm:register": "",
+         "fairterm:collocation": "",
+         "note": "",
+
+         }
+}
+
+list_values = {
+    "min:partOfSpeech":
+        {"NOUN": ""},
+    "basic:grammaticalGender":
+        {"Fem": ""},
+    "basic:grammaticalNumber":
+        {"Sing": ""},
+    "basic:termType":
+        {"complex term"}
+}
